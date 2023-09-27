904469d921877a04a45f5ac06de3888ab8ebe91e
777dan <danya.nikitin7@gmail.com>
2023-09-27 07:51:52 +0300

    the code was improved

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-let poets = {
-    "Evelyn Nightshade": [
-        "In a tent of colors, dark and deep",
-        "A fortune teller's secrets keep",
-        "With a deck of cards and a knowing eye",
-        "She unfolds destinies, reaching for the sky"
-    ],
-    "Lucius Starweaver": [
-        "With crystal ball and stars above",
-        "She speaks of destiny, dreams, and love",
-        "In her words, the future's spun",
-        "A mystic dance beneath the sun"
-    ],
-    "Seraphina Moonshadow": [
-        "Gazing into the great unknown",
-        "A fortune teller on her throne",
-        "She reads the signs, her wisdom clear",
-        "Guiding souls through hope and fear"
-    ]
-}
-
 let poem = "";
 function poet1() {
     poem = poets["Evelyn Nightshade"];
@@ -39,8 +18,8 @@ function divination() {
         let randomPhrase = Math.floor(Math.random() * poem.length);
         let answer = poem[randomPhrase];
         alert(answer);
-        let next = prompt("Any more divining?");
-        if (next == "no") {
+        let next = confirm("Any more divining?");
+        if (!next) {
             alert("Good bye!");
             break;
         }
